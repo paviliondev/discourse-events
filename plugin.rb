48aebf5fb56bdaba5c4fcdf8e1fd75bc22f427fe
Angus McLeod <angus@mcleod.org.au>
2026-07-29 14:44:51 +0200

    Bump plugin minor version

--- plugin.rb
+++ plugin.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 # name: discourse-events
 # about: Allows you to manage events in Discourse
-# version: 1.0.0
+# version: 1.1.0
 # authors: Angus McLeod
 # contact_emails: angus@angus.blog
 # url: https://github.com/angusmcleod/discourse-events
